b52723390fc85c5927feb19a7fbbc884c005e11f
leotangram <leotangram@gmail.com>
2018-03-03 14:01:05 -0500

    implementando create1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
   end  
+
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: product, status: 201, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end

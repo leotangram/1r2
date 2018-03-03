ce43c9849e36234a824cc57367616c6080c5a217
leotangram <leotangram@gmail.com>
2018-03-03 14:10:54 -0500

    actual

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,19 +3,4 @@ class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
   end  
-
-  def create
-    product = Product.new(products_params)
-    if product.save
-      render json: @product, status: 422
-    else
-      render json: { errors: product.errors }, status: 201
-    end
-  end
-
-  private
-  
-  def products_params
-    params.require(:product).permit(:name, :price)
-  end
 end

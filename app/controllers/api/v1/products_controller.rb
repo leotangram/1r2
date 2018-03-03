5f3eea6cd3b1ce23588877a75fbd925ed3ee2491
leotangram <leotangram@gmail.com>
2018-03-03 12:53:39 -0500

    probando funcionalidad de crear

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::ProductsController < ApplicationController
-
+  
   def index
     @products = Product.all
+  end
+  
+  def create
+    @product = product.new(product_params)
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
   end
   
 end

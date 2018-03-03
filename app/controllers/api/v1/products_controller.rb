a746a6ea905d47e70f096643089738d38d42d91d
leotangram <leotangram@gmail.com>
2018-03-03 13:26:33 -0500

    eliminando actual

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,34 +3,4 @@ class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
   end  
-  
-  def create
-    product = Product.new(products_params)
-    if product.save
-      render json: @product, status: 201
-    else
-      render json: { errors: product.errors }, status: 422
-    end
-  end
-  
-  def update
-    @product = Product.find(params[:id])
-    if(products_params[:name].present? && products_params[:price].present?)
-      @product.update(products_params)
-      render json: @product, status: 200
-    else
-      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-      render json: {errors:"Error 422"}, status: 422
-    end
-  end
-  
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-  end
-  
-  private
-  def products_params
-    params.require(:product).permit(:name, :price)
-  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,2 @@
 class Product < ActiveRecord::Base
-  validates :name, presence: true, on: :update 
-  validates :price, presence: true, on: :update
 end
